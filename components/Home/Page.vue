0c89a8f0caaa5883d7c4efd5ec22cf1e5c11db08
Valery <16lerochka@mail.ru>
2022-07-25 19:17:40 +0300

    add footer and main

--- components/Home/Page.vue
+++ components/Home/Page.vue
@@ -9,7 +9,7 @@
     </v-carousel>
     <v-banner
       single-line
-      elevation="0"
+      elevation="21"
       class="page-title text-center page-h6"
       color="grey darken-3"
     >
@@ -47,7 +47,7 @@
 
     <v-banner
       single-line
-      elevation="0"
+      elevation="21"
       class="page-title text-center page-h6 w-100"
       color="grey darken-3"
     >
@@ -61,7 +61,7 @@
           md="5"
           align-self="start"
         >
-          <v-card color="#616161" dark elevation="0">
+          <v-card color="#616161" dark elevation="0" class="color-card">
             <v-list-item three-line>
               <v-list-item-avatar tile size="90">
                 <v-icon style="font-size: 90px"
@@ -82,7 +82,70 @@
 
     <v-banner
       single-line
-      elevation="0"
+      elevation="21"
+      class="page-title text-center page-h6 w-100 mb-4"
+      color="grey darken-3"
+    >
+      <div>Сопровождение груза</div></v-banner
+    >
+
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-card dark color="#616161" max-width="900" elevation="0">
+            <v-img
+              :src="require(`~/assets/img/page_55.jpg`)"
+              height="500px"
+            ></v-img>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-list
+            dark
+            color="#616161"
+            class="color-card"
+            elevation="0"
+            three-line
+          >
+            <v-subheader>
+              Охранная Организация КГБ предлает множество услуг, связанных с
+              сопровождением:
+            </v-subheader>
+            <v-list-item v-for="(i, index) in listing" :key="index">
+              <v-list-item-content>
+                <v-list-item-title>{{ i.text }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-banner
+      single-line
+      elevation="21"
+      class="page-title text-center page-h6 w-100 mb-4"
+      color="grey darken-3"
+    >
+      <div>Охрана объекта</div></v-banner
+    >
+
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-card> card </v-card>
+        </v-col>
+        <v-col>
+          <v-card dark color="#616161" max-width="712" elevation="0">
+            <v-img :src="require(`~/assets/img/amg_cop.jpg`)"></v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-banner
+      single-line
+      elevation="21"
       class="page-title text-center page-h6 w-100"
       color="grey darken-3"
     >
@@ -92,12 +155,19 @@
     <v-container>
       <v-row>
         <v-col>
-          <v-card dark max-width="512">
-            <v-img :src="require(`~/assets/img/${security}.jpg`)"></v-img>
+          <v-card
+            dark
+            color="#616161"
+            max-width="712"
+            elevation="0"
+            height="400"
+            class="card-page color-card"
+          >
+            <v-img :src="require(`~/assets/img/${security}.png`)"></v-img>
           </v-card>
         </v-col>
         <v-col>
-          <v-card dark color="#616161" elevation="0">
+          <v-card dark color="#616161" elevation="0" class="color-card">
             <v-card-title>dasdadadada</v-card-title>
             <v-card-text
               >ОО КГБ обеспечит надежную личную охрануОО КГБ обеспечит надежную
@@ -117,16 +187,94 @@
 
     <v-banner
       single-line
-      elevation="0"
-      class="page-title text-center page-h6 w-100 mb-4"
+      elevation="21"
+      class="page-title text-center page-h6 w-100"
       color="grey darken-3"
     >
-      <div>Сопровождение груза</div></v-banner
+      <div>Вакансии</div></v-banner
     >
+
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-card dark color="#616161" elevation="0" class="color-card">
+            <v-list-item>
+              <v-list-item-content class="justify-center">
+                Наша команда ищет опытных охранников
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content class="justify-center">
+                8 (812) 224 24 96
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content class="justify-center">
+                Иванов Иван Иванович
+              </v-list-item-content>
+            </v-list-item>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-banner
+      single-line
+      elevation="21"
+      class="page-title text-center page-h6 w-100"
+      color="grey darken-3"
+    >
+      <div>Лицензии</div></v-banner
+    >
+
+    <div class="pt-5">
+      <carousel-3d :controlsVisible="true" height="494.27" display="5">
+        <slide :index="0"
+          ><v-img :src="require(`~/assets/img/lisenci/4.jpg`)"></v-img
+        ></slide>
+        <slide :index="1"
+          ><v-img :src="require(`~/assets/img/lisenci/5.jpg`)"></v-img
+        ></slide>
+        <slide :index="2"
+          ><v-img :src="require(`~/assets/img/lisenci/1.jpg`)"></v-img
+        ></slide>
+        <slide :index="3"
+          ><v-img :src="require(`~/assets/img/lisenci/2.jpg`)"></v-img
+        ></slide>
+        <slide :index="4"
+          ><v-img :src="require(`~/assets/img/lisenci/3.jpg`)"></v-img
+        ></slide>
+      </carousel-3d>
+    </div>
+
+    <v-banner
+      single-line
+      elevation="21"
+      class="page-title text-center page-h6 w-100"
+      color="grey darken-3"
+    >
+      <div>Наши социальные сети</div></v-banner
+    >
+    <v-container>
+      <v-row>
+        <v-col>
+          <v-card
+            dark
+            color="#616161"
+            elevation="0"
+            class="color-card"
+            height="200"
+          >
+            <v-card-title class="justify-center"> В проекте .... </v-card-title>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
+import { Carousel3d, Slide } from 'vue-carousel-3d'
 export default {
   name: 'HomePage',
   data() {
@@ -151,7 +299,7 @@ export default {
         },
         {
           title: 'Усовершенствованная логистика',
-          subtitle: 'Наша команда КГБ работает по всей россии ',
+          subtitle: 'Наша команда КГБ работает по всей России ',
         },
         {
           title:
@@ -159,8 +307,38 @@ export default {
           subtitle:
             'Команда КГБ может установить систему «умный дом» для охраны квартир дач и других объектов',
         },
+        {
+          title: 'Золотой месяц',
+          subtitle: 'Наша команда дает месяц отсрочки на оплату охранных услуг',
+        },
+      ],
+      listing: [
+        {
+          icon: '',
+          text: 'Для владельцев автомобиля',
+        },
+        {
+          icon: '',
+          text: 'Сопровождение груза',
+        },
+        {
+          icon: '',
+          text: 'Такси с защитой (аэропрт)',
+        },
+        {
+          icon: '',
+          text: 'Аренда авто',
+        },
+        {
+          icon: '',
+          text: 'Помощь в дороге автомобилисту ',
+        },
       ],
     }
+  },
+  components: {
+    Carousel3d,
+    Slide,
   },
 }
 </script>
--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,5 +1,29 @@
 <template>
-  <h1>Я нижняя часть экрана</h1>
+  <v-footer dark color="grey darken-3" class="justify-center">
+    <v-card
+      flat
+      tile
+      color="grey darken-3"
+      class="white--text text-center mx-auto"
+    >
+      <v-card-text class="white--text">
+        <v-img
+          :src="require('~/assets/img/logo.png')"
+          max-height="70"
+          max-width="70"
+          style="margin: 0 auto"
+        ></v-img>
+        <div class="pt-4">Телефон: 8 (812) 224 24 96</div>
+      </v-card-text>
+      <div class="pa-4">Информация на сайте не является публичной офертой</div>
+
+      <v-card-text class="white--text">
+        2015 — {{ new Date().getFullYear() }} © ООО "Охранная организация "Карго
+        Глобал Безопасность" Адрес: г. Санкт-Петербург ул. Большая Пороховская
+        61
+      </v-card-text>
+    </v-card>
+  </v-footer>
 </template>
 
 <script>
